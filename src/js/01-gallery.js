f86e759f4bc81df95259997fa4c31866384850d5
Alla-Goptsii <goptsy.a@gmail.com>
2022-08-21 20:58:27 +0300

    task-1 ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const currentContainer = document.querySelector('.gallery');
+const cardsGallery = addItemCard(galleryItems);
+
+currentContainer.insertAdjacentHTML('beforeend', cardsGallery);
+
+function addItemCard(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
