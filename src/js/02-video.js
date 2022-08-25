11b8ffb3b5c068e45ef04304767ecce1701c36f8
Alla-Goptsii <goptsy.a@gmail.com>
2022-08-25 19:10:46 +0300

    task-3 ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', throttle(onPlay, 500));
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0
+);
